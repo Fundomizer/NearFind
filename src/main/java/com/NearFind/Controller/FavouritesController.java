13729673dc48ed7e25cd544a24cb0e4207889fc6
Fundomizer <jesusjroway@gmail.com>
2025-10-17 19:24:28 +0800

    feat: Added placeholders for changing the tabs (technically it just changes the content in the center)

---
+++ src/main/java/com/NearFind/Controller/FavouritesController.java
@@ -0,0 +1,4 @@
+package com.NearFind.Controller;
+
+public class FavouritesController {
+}
--- src/main/java/com/NearFind/Controller/MainWindowController.java
+++ src/main/java/com/NearFind/Controller/MainWindowController.java
@@ -3,18 +3,38 @@ package com.NearFind.Controller;
 import com.NearFind.Utils.ViewUtils;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
 
 public class MainWindowController {
     @FXML
-    private VBox CenterPane;
+    private Pane CenterPane;
+
+    @FXML
+    public void initialize() {
+        System.out.println("Hello, view has been initialised");
+        onHome();
+    }
+
+    @FXML
+    private void onCompare() {
+        loadCenterContent("compare");
+    }
+    @FXML
+    private void onCart() {
+        loadCenterContent("cart");
+    }
+
+    @FXML
+    private void onFavourites () {
+        loadCenterContent("favourites");
+    }
 
     @FXML
     private void onHome() {
         loadCenterContent("home");
     }
 
-    public void loadCenterContent(String fxml) {
+    private void loadCenterContent(String fxml) {
         Node content = ViewUtils.loadFXML(fxml);
         CenterPane.getChildren().setAll(content);
     }
