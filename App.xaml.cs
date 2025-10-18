f605623d89026ef3b1139fd163edc1e79de6b330
Fundomizer <jesusjroway@gmail.com>
2025-10-18 21:04:42 +0800

    Fix: Set up the .NET MAUI files template. We can now start coding in C# MAUI framework

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NearFind
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NearFind
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
