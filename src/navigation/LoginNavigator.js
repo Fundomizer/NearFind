cc8506358fdaf3981a6fbfca13df522bc3f84fc4
Fundomizer <jesusjroway@gmail.com>
2025-11-23 22:41:00 +0800

    fix: Removed animation when navigating cuz it looks awful

--- src/navigation/LoginNavigator.js
+++ src/navigation/LoginNavigator.js
@@ -6,7 +6,7 @@ const Tab = createNativeStackNavigator();
 
 export default function LoginNavigator() {
     return (
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Navigator screenOptions={{ headerShown: false, animation: 'none' }}>
             <Tab.Screen name="Login" component={LoginScreen}></Tab.Screen>
             <Tab.Screen name="Signup" component={SignupScreen}></Tab.Screen>
         </Tab.Navigator>
